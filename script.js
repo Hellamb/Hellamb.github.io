e6913e8e703f4c66fb98e90291bf400de3c644ea
Hellamb <58140844+Hellamb@users.noreply.github.com>
2021-12-05 16:15:37 +0200

    fix algorithm and view

--- script.js
+++ script.js
@@ -70,12 +70,12 @@ $(document).ready(function(){
         },500);
         $("#game-board").removeClass('hidden');
         $(".light-field").animate({
-            "width": "130px",
-            "height": "130px"
+            "width": "100px",
+            "height": "100px"
         },500);
         $(".dark-field").animate({
-            "width": "130px",
-            "height": "130px"
+            "width": "100px",
+            "height": "100px"
         },500);
         //Передача хода белому
         playerTurn("W"); 
@@ -207,54 +207,41 @@ $(document).ready(function(){
         let utilityList2=[];
         //Заполнение списка возможными ходами
         nextTurnList = makeBoardsList(board, "B", -1, nextTurnList);
+        for(let i=0; i< nextTurnList.length; i++)
+        {
+            nextTurnList[i].parent = i;
+        }
         //Если есть победа на следующем ходу, то присваивать победу
         for(let i=0; i<nextTurnList.length ;i++){
             if(nextTurnList[i].fitness==0){showWin("B")}}
         // Заполнение списка возможных ходов противника
-        for(let i=0; i<nextTurnList.length ;i++){
-            utilityList1 = makeBoardsList(nextTurnList[i].board, "B", i, utilityList1);
-        }
+        deepTurnList = nextTurnList;
         let bestFit = 100;
         let bestBoardIndex = 0;
 
         if(difficulty>=2)
         {
+            for(let i=0; i<deepTurnList.length ;i++){
+                utilityList1 = makeBoardsList(deepTurnList[i].board, "W", i, utilityList1);
+            }
             for(let i=0; i<utilityList1.length ;i++){
-                utilityList2 = makeBoardsList(utilityList1[i].board, "W", utilityList1[i].parent, utilityList2);
+                utilityList2 = makeBoardsList(utilityList1[i].board, "B", utilityList1[i].parent, utilityList2);
             } 
-            utilityList1 = [];
-            for(let i=0; i<utilityList2.length ;i++){
-                utilityList1 = makeBoardsList(utilityList2[i].board, "B", utilityList2[i].parent, utilityList1);
-            }
-            utilityList2 = [];
+            deepTurnList = utilityList2;
         }
 
+        // if(difficulty>=3)
+        // {
+        //     for(let i=0; i<deepTurnList.length ;i++){
+        //         utilityList1 = makeBoardsList(deepTurnList[i].board, "W", i, utilityList1);
+        //     }
+        //     for(let i=0; i<utilityList1.length ;i++){
+        //         utilityList2 = makeBoardsList(utilityList1[i].board, "B", utilityList1[i].parent, utilityList2);
+        //     } 
+        //     deepTurnList = utilityList2;
+        // }
 
-        //Присваивает худшие значение возможному ходу
-        //худшими есть маленькие чётные, лучшими маленькие нечётные
-        for(i=0; i< utilityList1.length;i++)
-        {
-            let parentID = utilityList1[i].parent;
-            let fit = utilityList1[i].fitness;
-            let worst = worstFitnessInTurnArr[parentID];
-
-            if(worst == undefined
-                ||(fit%2==0 && (fit< worst || worst%2==1))
-                ||(worst%2==1 && fit>worst))
-            {worstFitnessInTurnArr[parentID] = fit;}
-        }
-
-        for(i=0;i < worstFitnessInTurnArr.length; i++)
-        {
-            let fit = worstFitnessInTurnArr[i];
-            if(  (bestBoardIndex==undefined && bestFit==undefined)
-                ||(fit%2==1 && (fit<bestFit || bestFit%2==0))
-                ||(bestFit%2==0 && bestFit<fit))
-            {
-                bestBoardIndex = i;
-                bestFit=fit;
-            }
-        }
+        bestBoardIndex = findIndex("B", deepTurnList)
         newBoard = nextTurnList[bestBoardIndex].board;
         let nID;
         let bID;
@@ -281,6 +268,141 @@ $(document).ready(function(){
         playerTurn("W");
     }
 
+    function findIndex(color, list)
+    {
+        let opColor;
+        if(color=='W'){opColor="B"};
+        if(color=='B'){opColor="W"};
+
+        let bestWorstPar = list[0].parent
+        let bestWorstFit = list[0].fitness
+        let lastParent = list[0].parent;
+        let worstFit = list[0].fitness
+        let isParentSkip = false;
+        for(let i=0; i<list.length;i++)
+        {
+            console.log(i)
+            let board = list[i].board;
+            if(isParentSkip && list[i].parent==lastParent){continue;}
+            else(isParentSkip && list[i].parent!=lastParent )
+            {
+                isParentSkip = false;
+            }
+            for(row=0;row<board.length;row++)
+            {
+                for(col=0; col<board[0].length;col++)
+                {
+                    let isBot = row>0 && board[row-1][col]==opColor && board[row][col]==color;
+                    let isTop = row<board.length-1 && board[row+1][col]==opColor && board[row][col]==color;
+                    let isLeft = col>0 && board[row][col-1]==opColor && board[row][col]==color;
+                    let isRight = col<board[0].length-1 && board[row][col+1]==opColor && board[row][col]==color;
+
+                    if(isBot)
+                    {
+                        let newBoard = JSON.parse(JSON.stringify(board));
+                        newBoard[row][col] = 'N';
+                        newBoard[row-1][col] = color;
+                        let fit = fitness(newBoard, color);
+                        let ut = utilityGetWorst(bestWorstPar, bestWorstFit, lastParent, worstFit, fit,list[i].parent);
+                        bestWorstFit = ut.bestWorstFit;
+                        bestWorstPar = ut.bestWorstPar;
+                        lastParent = ut.lastParent;
+                        worstFit = ut.worstFit;
+                        isParentSkip = ut.isParentSkip;
+                        if(isParentSkip){break}
+                    }
+                    if(isTop)
+                    {
+                        let newBoard = JSON.parse(JSON.stringify(board));
+                        newBoard[row][col] = 'N';
+                        newBoard[row+1][col] = color;
+                        let fit = fitness(newBoard, color);
+                        let ut = utilityGetWorst(bestWorstPar, bestWorstFit, lastParent, worstFit, fit,list[i].parent);
+                        bestWorstFit = ut.bestWorstFit;
+                        bestWorstPar = ut.bestWorstPar;
+                        lastParent = ut.lastParent;
+                        worstFit = ut.worstFit;
+                        isParentSkip = ut.isParentSkip;
+                        if(isParentSkip){break}
+                    }
+                    if(isLeft)
+                    {
+                        let newBoard = JSON.parse(JSON.stringify(board));
+                        newBoard[row][col] = 'N';
+                        newBoard[row][col-1] = color;
+                        let fit = fitness(newBoard, color);
+                        let ut = utilityGetWorst(bestWorstPar, bestWorstFit, lastParent, worstFit, fit,list[i].parent);
+                        bestWorstFit = ut.bestWorstFit;
+                        bestWorstPar = ut.bestWorstPar;
+                        lastParent = ut.lastParent;
+                        worstFit = ut.worstFit;
+                        isParentSkip = ut.isParentSkip;
+                        if(isParentSkip){break}
+                    }
+                    if(isRight)
+                    {
+                        let newBoard = JSON.parse(JSON.stringify(board));
+                        newBoard[row][col] = 'N';
+                        newBoard[row][col+1] = color;
+                        let fit = fitness(newBoard, color);
+                        let ut = utilityGetWorst(bestWorstPar, bestWorstFit, lastParent, worstFit, fit,list[i].parent);
+                        bestWorstFit = ut.bestWorstFit;
+                        bestWorstPar = ut.bestWorstPar;
+                        lastParent = ut.lastParent;
+                        worstFit = ut.worstFit;
+                        isParentSkip = ut.isParentSkip;
+                        if(isParentSkip){break}
+                    }
+                }
+                if(isParentSkip){break};
+            }
+            console.log("end")
+        }
+        return bestWorstPar;
+   }
+
+   function utilityGetWorst(bestWorstPar, bestWorstFit, lastParent, worstFit, fit, parent)
+   {
+        let isParentSkip = false;
+        if(parent !=lastParent)
+        {
+            let isBetter =    (worstFit%2==1 &&(bestWorstFit%2==0 || bestWorstFit>worstFit)) 
+                            ||(worstFit%2==0 && bestWorstFit%2==0 && worstFit>bestWorstFit)
+            if(isBetter)
+            {
+                bestWorstPar = lastParent;
+                bestWorstFit = worstFit; 
+            }
+            lastParent = parent;
+            worstFit = fit;
+        }else
+        {
+            let isWorse = (fit%2==0 && (fit<worstFit || worstFit%2==1)) || (worstFit %2==1 && fit>worstFit && fit%2==1);
+
+            if(isWorse)
+            {
+                worstFit = fit;
+                if(parent != 0)
+                {
+                    let isWorst = (fit%2==0 && (fit<bestWorstFit || bestWorstFit%2==1)) 
+                                || (bestWorstFit %2==1 && fit>bestWorstFit && fit%2==1);
+                    if(isWorst)
+                    {
+                        isParentSkip = true;
+                    }
+                }
+            }
+        }
+
+        return{
+            bestWorstPar,
+            bestWorstFit,
+            lastParent,
+            worstFit,
+            isParentSkip
+        }
+   }
+
     //Принимает доску, цвет, того, кто на ней ходит, нужного родителя и список для заполнения
     //Возвращает список возможных вариантов хода в объектах с родителем и ценностью
     function makeBoardsList(board, color, parent, list)
@@ -293,7 +415,12 @@ $(document).ready(function(){
         {
             for(col=0; col<board[0].length;col++)
             {
-                if(row>0 && board[row-1][col]==opColor && board[row][col]==color)
+                let isBot = row>0 && board[row-1][col]==opColor && board[row][col]==color;
+                let isTop = row<board.length-1 && board[row+1][col]==opColor && board[row][col]==color;
+                let isLeft = col>0 && board[row][col-1]==opColor && board[row][col]==color;
+                let isRight = col<board[0].length-1 && board[row][col+1]==opColor && board[row][col]==color;
+
+                if(isBot)
                 {
                     let newBoard = JSON.parse(JSON.stringify(board));
                     newBoard[row][col] = 'N';
@@ -301,7 +428,7 @@ $(document).ready(function(){
                     let fit = fitness(newBoard, color);
                     list.push(makeBoardObj(newBoard, parent, fit));
                 }
-                if(row<board.length-1 && board[row+1][col]==opColor && board[row][col]==color)
+                if(isTop)
                 {
                     let newBoard = JSON.parse(JSON.stringify(board));
                     newBoard[row][col] = 'N';
@@ -309,7 +436,7 @@ $(document).ready(function(){
                     let fit = fitness(newBoard, color);
                     list.push(makeBoardObj(newBoard, parent, fit));
                 }
-                if(col>0 && board[row][col-1]==opColor && board[row][col]==color)
+                if(isLeft)
                 {
                     let newBoard = JSON.parse(JSON.stringify(board));
                     newBoard[row][col] = 'N';
@@ -317,7 +444,7 @@ $(document).ready(function(){
                     let fit = fitness(newBoard, color);
                     list.push(makeBoardObj(newBoard, parent, fit));
                 }
-                if(col<board[0].length-1 && board[row][col+1]==opColor && board[row][col]==color)
+                if(isRight)
                 {
                     let newBoard = JSON.parse(JSON.stringify(board));
                     newBoard[row][col] = 'N';
@@ -353,19 +480,13 @@ $(document).ready(function(){
         {
             for(j=0 ;j<board[0].length; j++)
             {
-                if ((  i>0 
-                    && board[i-1][j]==color 
-                    && board[i][j]==opColor)
-                    || (i<board.length-1 
-                    && board[i+1][j]==color
-                    && board[i][j]==opColor)
-                    || (j>0 
-                    && board[i][j-1]==color
-                    && board[i][j]==opColor)
-                    || (j<board[0].length-1 
-                    && board[i][j+1]==color 
-                    && board[i][j]==opColor))
-                    {counter++}
+                let isBot =i>0 && board[i-1][j]==color && board[i][j]==opColor;
+                let isTop =i<board.length-1 && board[i+1][j]==color && board[i][j]==opColor;
+                let isLeft= j>0 && board[i][j-1]==color && board[i][j]==opColor;
+                let isRight =j<board[0].length-1 && board[i][j+1]==color && board[i][j]==opColor;
+
+                if (isBot|| isTop ||isLeft||isRight){
+                    counter++}
             }
         }
         return counter;
